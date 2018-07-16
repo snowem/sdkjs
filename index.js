805a840a5ba81c3c1f1c8d5eb04cf2b1b5cd603d
Jackie Dinh <trinh.tuyet158@gmail.com>
2018-07-16 18:17:02 +0800

    add certificate files

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ var https = require('https');
 var fs = require('fs');
 
 var options = {
-    key: fs.readFileSync('./example.key'),
-    cert: fs.readFileSync('./example.crt'),
+    key: fs.readFileSync('./js/example.key'),
+    cert: fs.readFileSync('./js/example.crt'),
     requestCert: false,
     rejectUnauthorized: false
 };
 
 var app = express()
-
-//app.use('/',express.static('.'))
-//app.listen(8000, function () {
-//  console.log('Snowem demo app listening on port 8000!')
-//})
-
 app.use('/',express.static('.'))
 var server = https.createServer(options, app).listen(8000, function(){
   console.log('Snowem demo app listening on port 8000!')
