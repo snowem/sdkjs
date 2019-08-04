a88d1717664d04b94d5937272c1f0e64aeea388a
Jackie Dinh <jackie@ai-lab>
2019-08-04 17:56:08 +0800

    setup webpack + es6 + lib used in browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
 require("babel-register");
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
+const env = require('yargs').argv.env;
+const pkg = require('./package.json');
+//const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
 
 const config = {
+  mode: mode,
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, './example'),
+    filename: outputFile,
+    library: 'snowem',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
   },
   module: {
     rules : [
@@ -21,12 +42,16 @@ const config = {
       }
     ]
   },
-  plugins: [
+  /*plugins: [
     new HtmlWebpackPlugin({
       template: './example/index.html',
       filename: 'index.html',
       hash: true
     })
-  ]
+  ]*/
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
 };
 module.exports = config;
